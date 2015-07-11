f6ffe256d59b697eaf9775bf4a3f892144b797a9
lan496 <kohei19950508@gmail.com>
2015-07-11 14:14:23 +0900

    return intractive shell when syntax occurs

--- main.ml
+++ main.ml
@@ -2,14 +2,22 @@ open Syntax
 open Eval
 
 let rec read_eval_print env =
-  print_string "# ";
-  flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-    Printf.printf "val %s = " id;
-    pp_val v;
-    print_newline();
-    read_eval_print newenv
+    print_string "# ";
+    flush stdout;
+    let decl = 
+        try
+            Parser.toplevel Lexer.main (Lexing.from_channel stdin)
+        with
+            _ ->
+                Printf.printf "Syntax Error";
+                print_newline();
+                read_eval_print env
+    in
+    let (id, newenv, v) = eval_decl env decl in
+        Printf.printf "val %s = " id;
+        pp_val v;
+        print_newline();
+        read_eval_print newenv
 
 let initial_env = 
     Environment.extend "i" (IntV 1)
